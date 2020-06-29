3b0bd24aff075c8f74317c33cfca0532c8c6407e
02-siri <67592378+02-siri@users.noreply.github.com>
2020-06-29 20:18:39 +0530

    Update sizeofdatatypes.c

--- sizeofdatatypes.c
+++ sizeofdatatypes.c
@@ -8,3 +8,16 @@ int main()
 	cout<<"Size of double:"<<sizeof(double)<<"bytes"<<endl;
 	return 0;
 }
+
+//write a c++ program to find the product of the two numbersentered by the user
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b;
+	cout<<"enter the two numbers"<< endl;
+	cin>>a>>b;
+	cout<<"Product="<<a*b<<endl;
+	return 0;
+}
+
